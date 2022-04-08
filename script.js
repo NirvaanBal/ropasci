5020b49ea2ac609c164e54c0e08bd02942d01c10
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-04-08 14:39:25 +0530

    Add game logic

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-const computerPlay = () => {
+function computerPlay() {
     const actions = ['rock', 'paper', 'scissors'];
 
     const random = Math.floor(Math.random() * 3);
 
     return actions[random];
-};
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You Win! Rock beats Scissors';
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You Lose! Paper beats Rocks';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You Win! Paper beats Rock';
+    } else if (
+        playerSelection === 'paper' &&
+        computerSelection === 'scissors'
+    ) {
+        return 'You Lose! Scissors beat Paper';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You Lose! Rock beats Scissors';
+    } else if (
+        playerSelection === 'scissors' &&
+        computerSelection === 'paper'
+    ) {
+        return 'You Win! Scissors beat Rock';
+    }
+}
